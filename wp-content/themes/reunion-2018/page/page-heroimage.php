07b4aed58be995d8699acf16be40972624713034
Bryan Davis <bdavis@artandbusinessone.com>
2018-12-11 15:01:52 -0700

    adjust the background position on hermitages for subpages

--- wp-content/themes/reunion-2018/page/page-heroimage.php
+++ wp-content/themes/reunion-2018/page/page-heroimage.php
@@ -1,4 +1,4 @@
   <section class="pagehero-section">
-    <div class="page-heroimage" style="background: url('<?php echo get_the_post_thumbnail_url($post->ID); ?>') no-repeat center top; background-size: cover;"></div>
+    <div class="page-heroimage" style="background: url('<?php echo get_the_post_thumbnail_url($post->ID); ?>') no-repeat center 50%; background-size: cover;"></div>
     <div class="scroll-indicator"></div>
   </section>
