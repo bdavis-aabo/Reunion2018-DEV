912fbc0eb6530e91dfbfa70979bd20ffd15efd3b
Bryan Davis <bdavis@artandbusinessone.com>
2018-12-21 15:57:35 -0700

    remove hero image code if none exists.

--- wp-content/themes/reunion-2018/page/page-heroimage.php
+++ wp-content/themes/reunion-2018/page/page-heroimage.php
@@ -1,4 +1,7 @@
+
+<?php if(get_the_post_thumbnail_url($post->ID) != ''): ?>
   <section class="pagehero-section">
     <div class="page-heroimage" style="background: url('<?php echo get_the_post_thumbnail_url($post->ID); ?>') no-repeat"></div>
     <div class="scroll-indicator"></div>
   </section>
+<?php endif; ?>
