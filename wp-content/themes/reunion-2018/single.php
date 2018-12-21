f042065dccf980e3bd881bbb76fbae3a599e7eda
Bryan Davis <bdavis@artandbusinessone.com>
2018-12-21 16:31:36 -0700

    remove button from single post.

--- wp-content/themes/reunion-2018/single.php
+++ wp-content/themes/reunion-2018/single.php
@@ -13,7 +13,6 @@
             <p class="article-date">Posted on <?php the_time('d') ?> <?php the_time('M Y') ?></p>
 
             <?php the_content() ?>
-            <a href="<?php the_permalink() ?>" title="<?php the_title() ?>" class="btn gold-btn">Read More <i class="fas fa-chevron-right"></i></a>
           </article>
         <?php endwhile; endif; ?>
         </div>
